d692e8e27106ce38c227ae0c0f810e12f7bde9fb
Mihir Chachar <mchachar143@gmail.com>
2025-12-04 19:06:38 +0000

    fix done

--- app.R
+++ app.R
@@ -1,5 +1,2 @@
 
-rsconnect::setAccountInfo(name='1kxoci-dr0mihir-chachar', token='EAF0D0B975ED744BE94F791CD965345A', secret='CYT22m8dTPhbuqYwr32yCk5MRzb4hh7qbfT9jSek')
 
-library(rsconnect)
-rsconnect::deployApp('https://github.com/Mihir2728/Shiny_Assignment.git')
