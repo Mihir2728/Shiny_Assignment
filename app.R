8919cf5c70eb0456bb49e1741be1be76978da361
Mihir Chachar <mchachar143@gmail.com>
2025-12-04 18:56:10 +0000

    link changed

--- app.R
+++ app.R
@@ -2,4 +2,4 @@
 rsconnect::setAccountInfo(name='1kxoci-dr0mihir-chachar', token='EAF0D0B975ED744BE94F791CD965345A', secret='CYT22m8dTPhbuqYwr32yCk5MRzb4hh7qbfT9jSek')
 
 library(rsconnect)
-rsconnect::deployApp('/Users/drmihirchachar/shiny_git/Shiny_Assignment')
+rsconnect::deployApp('https://github.com/Mihir2728/Shiny_Assignment.git')
