39755eec4010535cd5a18fddcc00e8a9e25b90d6
Mihir Chachar <mchachar143@gmail.com>
2025-12-04 18:47:17 +0000

    package installation deleted

--- app.R
+++ app.R
@@ -1,4 +1,3 @@
-install.packages('rsconnect')
 
 rsconnect::setAccountInfo(name='1kxoci-dr0mihir-chachar', token='EAF0D0B975ED744BE94F791CD965345A', secret='CYT22m8dTPhbuqYwr32yCk5MRzb4hh7qbfT9jSek')
 
