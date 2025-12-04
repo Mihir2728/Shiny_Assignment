6496f9c1c6e0a7d509d87c889b37ac632baa77fe
Mihir Chachar <mchachar143@gmail.com>
2025-12-04 18:26:25 +0000

    path change

--- app.R
+++ app.R
@@ -3,4 +3,4 @@ install.packages('rsconnect')
 rsconnect::setAccountInfo(name='1kxoci-dr0mihir-chachar', token='EAF0D0B975ED744BE94F791CD965345A', secret='CYT22m8dTPhbuqYwr32yCk5MRzb4hh7qbfT9jSek')
 
 library(rsconnect)
-rsconnect::deployApp('path/to/your/app')
+rsconnect::deployApp('/Users/drmihirchachar/shiny_git/Shiny_Assignment')
